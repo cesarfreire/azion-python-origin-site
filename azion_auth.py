23bb560b484af67e3c1af71d96e288387533a0d9
César Freire <iceesar@live.com>
2022-02-01 09:19:17 -0300

    first comit

--- azion_auth.py
+++ azion_auth.py
@@ -7,7 +7,7 @@ class AzionAuth:
     @staticmethod
     def autenticacao():
         cabecalho = {'Accept': 'application/json; version=3',
-                   'Authorization': 'Basic Y2VzYXIuZnJlaXJlQG1hZ2Ftb2JpLmNvbS5icjpNYWlHbG9jazltbUA='}
+                   'Authorization': 'Basic XXXXXXXX'}
         requisicao = requests.post("https://api.azionapi.net/tokens", '', headers=cabecalho)
         token = json.loads(requisicao.text)
         print('Token: ' + token['token'])
